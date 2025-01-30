7b249ff437ea5ce3cb8eaac03a60fecfffafe37b
SchonnMike <schonnmike@gmail.com>
2025-01-30 21:40:26 +0800

    Combined function codes and changed ZeroDivisionError case for zero denominator

--- Fraction.py
+++ Fraction.py
@@ -1,22 +1,48 @@
 class Fraction(object):
 
     def __init__(self, numerator=0, denominator=1):
-        self.numerator = numerator
-        self.denominator = denominator
-
+        # Defaults to 0, 1 for invalid inputs
+        if isinstance(numerator, str):
+            self.numerator, self.denominator = self.find_fraction_from_string(numerator)
+        elif isinstance(numerator, float) or isinstance(denominator, float):   # Floats are not supported
+            self.numerator, self.denominator = 0, 1   
+        else:
+            self.numerator, self.denominator = numerator, denominator
+        
+        if self.denominator == 0:
+            #self.numerator, self.denominator = 0, 1
+            raise ZeroDivisionError("Denominator cannot be 0")
+    
+    def find_fraction_from_string(self, fraction_string):
+        # Returns 0, 1 for invalid strings
+        fraction_string = fraction_string.strip()
+        
+        if fraction_string.count('/') != 1:
+            return 0, 1   
+        
+        try:
+            numerator, denominator = fraction_string.split('/')
+            numerator, denominator = int(numerator), int(denominator)
+            return numerator, denominator
+        except ValueError:
+            return 0, 1
+    
     def gcd(a, b):
-        if a or b != 0:
-            #TODO
-        pass
+        if a == 0 or b == 0:
+            return 0
+
+        while b != 0:
+            a, b = b, a % b
+
+        return abs(a)
 
     def get_numerator(self):
-        #TODO
-        pass
+        return str(self.numerator)
 
     def get_denominator(self):
-        #TODO
-        pass
+        return str(self.denominator)
 
     def get_fraction(self):
-        #TODO
-        pass
+        if self.denominator == 1:
+            return str(self.numerator)
+        return f"{self.numerator}/{self.denominator}"
