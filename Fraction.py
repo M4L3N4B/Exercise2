2b16c6b67a37c8517a48763f6a01b169cdd5a6ce
M4L3N4B <caikelvin0315@gmail.com>
2025-01-30 20:39:33 +0800

    Implemented fraction constructions for integer inputs, invalid cases

--- Fraction.py
+++ Fraction.py
@@ -1,11 +1,19 @@
 class Fraction(object):
 
     def __init__(self, numerator=0, denominator=1):
-        # Convert proper string argument to integers
+        # Defaults to 0, 1 for invalid inputs
         if isinstance(numerator, str):
             self.numerator, self.denominator = self.find_fraction_from_string(numerator)
+        elif isinstance(numerator, float) or isinstance(denominator, float):   # Floats are not supported
+            self.numerator, self.denominator = 0, 1   
+        else:
+            self.numerator, self.denominator = numerator, denominator
+        
+        if self.denominator == 0:
+            self.numerator, self.denominator = 0, 1
     
     def find_fraction_from_string(self, fraction_string):
+        # Returns 0, 1 for invalid strings
         fraction_string = fraction_string.strip()
         
         if fraction_string.count('/') != 1:
