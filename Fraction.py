d14d5449391c44c982838dcaec97b59cf123762f
M4L3N4B <caikelvin0315@gmail.com>
2025-01-30 17:49:35 +0800

    Added fraction initialization from string input

--- Fraction.py
+++ Fraction.py
@@ -1,12 +1,25 @@
 class Fraction(object):
 
     def __init__(self, numerator=0, denominator=1):
-        self.numerator = numerator
-        self.denominator = denominator
-
+        # Convert proper string argument to integers
+        if isinstance(numerator, str):
+            self.numerator, self.denominator = self.find_fraction_from_string(numerator)
+    
+    def find_fraction_from_string(self, fraction_string):
+        fraction_string = fraction_string.strip()
+        
+        if fraction_string.count('/') != 1:
+            return 0, 1   
+        
+        try:
+            numerator, denominator = fraction_string.split('/')
+            numerator, denominator = int(numerator), int(denominator)
+            return numerator, denominator
+        except ValueError:
+            return 0, 1
+    
     def gcd(a, b):
-        if a or b != 0:
-            #TODO
+        #TODO
         pass
 
     def get_numerator(self):
