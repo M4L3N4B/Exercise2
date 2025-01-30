5f76d34d3640d86f7314f97fe697e11c73820f6a
SchonnMike <schonnmike@gmail.com>
2025-01-30 22:26:02 +0800

    Added lowest_term function and removed comments

--- Fraction.py
+++ Fraction.py
@@ -1,20 +1,20 @@
 class Fraction(object):
 
     def __init__(self, numerator=0, denominator=1):
-        # Defaults to 0, 1 for invalid inputs
         if isinstance(numerator, str):
             self.numerator, self.denominator = self.find_fraction_from_string(numerator)
-        elif isinstance(numerator, float) or isinstance(denominator, float):   # Floats are not supported
+        elif isinstance(numerator, float) or isinstance(denominator, float):
             self.numerator, self.denominator = 0, 1   
         else:
             self.numerator, self.denominator = numerator, denominator
         
         if self.denominator == 0:
-            #self.numerator, self.denominator = 0, 1
             raise ZeroDivisionError("Denominator cannot be 0")
+
+        if self.denominator != 0:
+            self.lowest_term()
     
     def find_fraction_from_string(self, fraction_string):
-        # Returns 0, 1 for invalid strings
         fraction_string = fraction_string.strip()
         
         if fraction_string.count('/') != 1:
@@ -26,7 +26,8 @@ class Fraction(object):
             return numerator, denominator
         except ValueError:
             return 0, 1
-    
+        
+    @staticmethod
     def gcd(a, b):
         if a == 0 or b == 0:
             return 0
@@ -35,6 +36,17 @@ class Fraction(object):
             a, b = b, a % b
 
         return abs(a)
+
+    def lowest_term(self):
+        gcd_value = self.gcd(abs(self.numerator), abs(self.denominator))
+        
+        if gcd_value != 0:
+            self.numerator = self.numerator // gcd_value
+            self.denominator = self.denominator // gcd_value
+            
+        if self.denominator < 0:
+            self.numerator = -self.numerator
+            self.denominator = -self.denominator
 
     def get_numerator(self):
         return str(self.numerator)
@@ -45,4 +57,5 @@ class Fraction(object):
     def get_fraction(self):
         if self.denominator == 1:
             return str(self.numerator)
+        
         return f"{self.numerator}/{self.denominator}"
